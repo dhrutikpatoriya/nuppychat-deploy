bc5a7a7cf38b7bc70907ff3489b65c75cd268904
Dhrutik Patoriya <dhrutikpatoriya@Dhrutiks-MacBook-Air.local>
2026-03-10 15:41:19 +0530

    Fix postinstall patch-package error on Render

--- backand/server.js
+++ backand/server.js
@@ -1,4 +1,4 @@
-import dotenv from "dotenv" 
+import dotenv from "dotenv"
 dotenv.config();
 import cors from 'cors'
 import chatRouter from "./routes/chat.route.js";
@@ -8,20 +8,29 @@ import authRouter from "./routes/auth.route.js";
 import { dbConnection } from "./lid/db.js";
 import cookieParser from "cookie-parser";
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5001;
 
 app.use(cors({
-  origin:"http://localhost:5173",
-  credentials:true
+  origin: process.env.CLIENT_URL || "http://localhost:5173",
+  credentials: true
 }))
 app.use(express.json())
 app.use(cookieParser())
-app.use("/api/auth",authRouter);
-app.use("/api/user",userRouter);
-app.use("/api/chat",chatRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
+app.use("/api/chat", chatRouter);
 
+import path from "path";
 
+const __dirname = path.resolve();
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 
 app.listen(PORT, () => {
